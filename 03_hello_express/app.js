39a379348954d403d3371ff147aa5bf1bab643a3
Raghu Ugare <raghu@votercircle.com>
2015-08-11 10:40:32 +0530

    Added a 404 response to 'unknown' page URL's

--- 03_hello_express/app.js
+++ 03_hello_express/app.js
@@ -5,5 +5,9 @@ app.get('/', function(req, res){
     res.send('Hello World! :~)');
 });
 
+app.get('*', function(req, res) {
+    res.send(404, 'Page not found! :~(');
+});
+
 app.listen(8080);
 console.log('Express server listening on port 8080...');
