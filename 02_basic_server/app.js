3a3f536f10d6e8ca8dd214cd358eaaa799d9b25e
Raghu Ugare <raghu.ugare@gmail.com>
2016-12-22 19:14:38 +0530

    Fix typo.

--- 02_basic_server/app.js
+++ 02_basic_server/app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-    response.write(200, {"Content-type": "plain/text"});
-    response.end("Hello, World!\n");
+    res.write(200, {"Content-type": "plain/text"});
+    res.end("Hello, World!\n");
 });
 
 console.log("Server listening on port 8080...");
